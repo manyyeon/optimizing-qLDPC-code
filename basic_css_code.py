bc28796c18afd3823a91646283e133760082f6ab
manyyeon <udayeon@naver.com>
2025-07-24 15:00:31 -0600

    add construct HGP code

--- basic_css_code.py
+++ basic_css_code.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_matrix, hstack, kron, eye, block_diag
 from typing import Tuple
-from logical_operators import get_logical_operators
+from logical_operators import get_logical_operators, get_logical_operators_by_pivoting
 
 def repetition_code(distance: int) -> csr_matrix:
     row_ind, col_ind = zip(
@@ -9,6 +9,26 @@ def repetition_code(distance: int) -> csr_matrix:
     )
     data = np.ones(2 * distance, dtype=np.uint8)
     return csr_matrix((data, (row_ind, col_ind)))
+
+def construct_HGP_code(H: np.ndarray) -> csr_matrix:
+    """
+    Constructs the HGP code from a given classical parity-check matrix H.
+    """
+    m, n = H.shape
+    # Convert H to sparse matrix
+    H_sparse = csr_matrix(H, dtype=np.uint8)
+    Im = eye(m, dtype=np.uint8)
+    In = eye(n, dtype=np.uint8)
+
+    Hx = csr_matrix(hstack([kron(H, In), kron(Im, H.T)], dtype=np.uint8))
+    Hz = csr_matrix(hstack([kron(In, H), kron(H.T, Im)], dtype=np.uint8))
+
+    # print(f"Hx shape: {Hx.shape}")
+    # print(f"Hz shape: {Hz.shape}")  
+    # print(f"Hx:\n{Hx.toarray()}")
+    # print(f"Hz:\n{Hz.toarray()}")
+
+    return Hx, Hz
 
 def toric_code_matrices(
     distance: int,
@@ -49,7 +69,17 @@ def toric_code_matrices_with_logical_operators_by_algorithm(
     Hx = csr_matrix(hstack([kron(H, e), kron(e, H.T)], dtype=np.uint8))
     Hz = csr_matrix(hstack([kron(e, H), kron(H.T, e)], dtype=np.uint8))
 
-    Lx, Lz = get_logical_operators(np.array(Hx.toarray())[:-1, :], np.array(Hz.toarray())[:-1, :])
+    # Lx, Lz = get_logical_operators(np.array(Hx.toarray())[:-1, :], np.array(Hz.toarray())[:-1, :])
+    Lx, Lz = get_logical_operators_by_pivoting(np.array(Hx.toarray())[:-1, :], np.array(Hz.toarray())[:-1, :])
+
+    print(f"Hx shape: {Hx.shape}")
+    print(f"Hx: \n{Hx.toarray()}")
+    print(f"Hz shape: {Hz.shape}")
+    print(f"Hz: \n{Hz.toarray()}")
+    print(f"Lx shape: {Lx.shape}")
+    print(f"Lx: \n{Lx.toarray()}")
+    print(f"Lz shape: {Lz.shape}")
+    print(f"Lz: \n{Lz.toarray()}")
 
     Lx = csr_matrix(Lx, dtype=np.uint8)
     Lz = csr_matrix(Lz, dtype=np.uint8)
@@ -88,10 +118,25 @@ if __name__ == "__main__":
     distance = 3  # Example distance
     # Hx, Hz, Lx, Lz = toric_code_matrices(distance)
     # Hx, Hz, Lx, Lz = toric_code_matrices_with_logical_operators_by_algorithm(distance)
-    Hx, Hz, Lx, Lz = shor_code_matrices()
+    # Hx, Hz, Lx, Lz = shor_code_matrices()
 
-    print(f"Hx:\n{Hx.toarray()}")
-    print(f"Hz:\n{Hz.toarray()}")
-    print(f"Lx:\n{Lx.toarray()}")
-    print(f"Lz:\n{Lz.toarray()}")
+    import numpy as np
+
+    H = np.array([
+        [1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1],
+        [0, 1, 1, 1, 1, 0]
+    ], dtype=np.uint8)
+
+    Hx, Hz = construct_HGP_code(H)
+
+    print(f"weight of each row in Hx: {Hx.sum(axis=1)}")
+    print(f"weight of each col in Hx: {Hx.sum(axis=0)}")
+    print(f"weight of each row in Hz: {Hz.sum(axis=1)}")
+    print(f"weight of each col in Hz: {Hz.sum(axis=0)}")
+
+    # print(f"Hx:\n{Hx.toarray()}")
+    # print(f"Hz:\n{Hz.toarray()}")
+    # print(f"Lx:\n{Lx.toarray()}")
+    # print(f"Lz:\n{Lz.toarray()}")
 
