e9525880c9eda7d4ad715ddc6a05889f4b8a929a
manyyeon <udayeon@naver.com>
2025-07-28 18:32:37 -0600

    analyze best from random walk

--- optimization/analyze_codes/analyze_best_from_PE.py
+++ optimization/analyze_codes/analyze_best_from_PE.py
@@ -13,7 +13,10 @@ import numpy as np
 
 grpname = codes
 p_vals = np.logspace(-2, -1, 20)
-MC_budget = int(1e3)
+MC_budget = int(1e5)
+
+input_file_path = "optimization/results/best_from_random_walk.hdf5"
+output_file_path = "optimization/results/analysis_best_from_random_walk.hdf5"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -25,25 +28,21 @@ if __name__ == '__main__':
 
     fn_data = {}
 
-    with h5py.File('optimization/results/random_exploration_3.hdf5', 'r') as f:
-        best_state_edge_list, _ = min(((s, v) for s, v in zip(f[grpname[C]]['states'], f[grpname[C]]['logical_error_rates'])), key=lambda x: x[1])
-        index_of_min = np.argmin(f[grpname[C]]['logical_error_rates'])
-        print(f"Minimum logical error rate found in state {index_of_min} with value {f[grpname[C]]['logical_error_rates'][index_of_min]}")
+    with h5py.File(input_file_path, 'r') as f:
+        best_state_edge_list, _ = min(((s, v) for s, v in zip(f[grpname[C]]['states'], f[grpname[C]]['values'])), key=lambda x: x[1])
+        index_of_min = np.argmin(f[grpname[C]]['values'])
+        print(f"Minimum logical error rate found in state {index_of_min} with value {f[grpname[C]]['values'][index_of_min][0]}")
         best_state = from_edgelist(best_state_edge_list)
 
-    logical_error_rates = compute_decoding_performance_from_state(best_state, p_vals, MC_budget)
+    cost_result = compute_decoding_performance_from_state(best_state, p_vals, MC_budget, bp_max_iter=None, run_label="Best state from Random Walk Exploration")
 
     best_state_edge_list = np.row_stack(best_state_edge_list, dtype=np.uint8)
-    logical_error_rates = np.row_stack(logical_error_rates, dtype=np.float64)
+    logical_error_rates = np.row_stack(cost_result['logical_error_rates'], dtype=np.float64)
 
-    with h5py.File("optimization/results/best_from_exploration.hdf5", "a") as f:
+    with h5py.File(output_file_path, "a") as f:
         grp = f.require_group(grpname[C])
         grp.attrs['MC_budget'] = MC_budget
 
-        if "best_state" in grp:
-            del grp["best_state"]
-        grp.create_dataset("best_state", data=best_state_edge_list, shape=(1, best_state_edge_list.shape[0]))
-        
         if "physical_error_rates" in grp:
             del grp["physical_error_rates"]
         grp.create_dataset("physical_error_rates", data=p_vals)
@@ -51,4 +50,14 @@ if __name__ == '__main__':
         if "logical_error_rates" in grp:
             del grp["logical_error_rates"]
         grp.create_dataset("logical_error_rates", data=logical_error_rates)
+
+        if "logical_error_rates_std" in grp:
+            del grp["logical_error_rates_std"]
+        grp.create_dataset("logical_error_rates_std", data=cost_result['stds'])
+
+        if "runtimes" in grp:
+            del grp["runtimes"]
+        grp.create_dataset("runtimes", data=cost_result['runtimes'])
+
+    print(f"Analysis of best state from random walk exploration for code family {codes[C]} completed and saved to {output_file_path}.")
     
