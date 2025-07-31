fix the error of the std error computation

optimization/analyze_codes/analyze_best_from_Greedy.py
@@ -12,12 +12,12 @@ import numpy as np
 
 grpname = codes
 p_vals = np.logspace(-2, -1, 20)
-MC_budget = int(1e5)
+MC_budget = int(1e6)
 
 # names = ["PEG_codes", "SA_codes", "PS_codes", "PE_codes"]
 names = ["PEG_codes"]
-input_file = "optimization/results/greedy_exploration_run8.hdf5"
-output_file = "optimization/results/analysis_best_from_greedy_bpmaxiter2.hdf5"
+input_file = "optimization/results/greedy_exploration_bpmaxiter_62_run4.hdf5"
+output_file = "optimization/results/analysis_best_from_greedy_bpmaxiter_62_run4_2_MC_1e6.hdf5"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         best_state = from_edgelist(best_state_edge_list)
 
     cost_result = compute_decoding_performance_from_state(best_state, p_vals, MC_budget, 
-                                                            bp_max_iter=2, run_label="Best state from Greedy Exploration")
+                                                            bp_max_iter=None, run_label="Best state from Greedy Exploration")
 
     logical_error_rates = np.row_stack(cost_result['logical_error_rates'], dtype=np.float64)
 
@@ -52,9 +52,9 @@ if __name__ == '__main__':
             del grp['logical_error_rates']
         grp.create_dataset("logical_error_rates", data=logical_error_rates)
 
-        if 'logical_error_rates_std' in grp:
-            del grp['logical_error_rates_std']
-        grp.create_dataset("logical_error_rates_std", data=cost_result['stds'])
+        if 'logical_error_rates_stderr' in grp:
+            del grp['logical_error_rates_stderr']
+        grp.create_dataset("logical_error_rates_stderr", data=cost_result['stderrs'])
 
         if 'runtimes' in grp:
             del grp['runtimes']

optimization/analyze_codes/analyze_best_from_PE.py
@@ -13,10 +13,10 @@ import numpy as np
 
 grpname = codes
 p_vals = np.logspace(-2, -1, 20)
-MC_budget = int(1e5)
+MC_budget = int(1e6)
 
 input_file_path = "optimization/results/best_from_random_walk.hdf5"
-output_file_path = "optimization/results/analysis_best_from_random_walk.hdf5"
+output_file_path = "optimization/results/analysis_best_from_random_walk_MC_1e6.hdf5"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -51,9 +51,9 @@ if __name__ == '__main__':
             del grp["logical_error_rates"]
         grp.create_dataset("logical_error_rates", data=logical_error_rates)
 
-        if "logical_error_rates_std" in grp:
-            del grp["logical_error_rates_std"]
-        grp.create_dataset("logical_error_rates_std", data=cost_result['stds'])
+        if "logical_error_rates_stderr" in grp:
+            del grp["logical_error_rates_stderr"]
+        grp.create_dataset("logical_error_rates_stderr", data=cost_result['stderrs'])
 
         if "runtimes" in grp:
             del grp["runtimes"]

optimization/analyze_codes/decoder_performance_from_state.py
@@ -40,7 +40,7 @@ def compute_decoding_performance_from_state(state: nx.MultiGraph, p_vals: np.nda
     Lx, Lz = get_logical_operators_by_pivoting(Hx, Hz)
 
     logical_error_rates = []
-    stds = []
+    stderrs = []
     runtimes = []
 
     print(f"BP max iterations: {bp_max_iter}, OSD order: {osd_order}, MS scaling factor: {ms_scaling_factor}")
@@ -59,15 +59,15 @@ def compute_decoding_performance_from_state(state: nx.MultiGraph, p_vals: np.nda
             osd_order=osd_order,
         )
 
-        logical_error_rate, std, runtime = compute_logical_error_rate(Hz, Lz, p, run_count=MC_budget, DECODER=bp_osd_decoder, run_label=run_label, DEBUG=False)
+        logical_error_rate, stderr, runtime = compute_logical_error_rate(Hz, Lz, p, run_count=MC_budget, DECODER=bp_osd_decoder, run_label=run_label, DEBUG=False)
         print(f"Logical error rate for p={p}: {logical_error_rate}")
         
         logical_error_rates.append(logical_error_rate)
-        stds.append(std)
+        stderrs.append(stderr)
         runtimes.append(runtime)
 
     return {
         "logical_error_rates": logical_error_rates,
-        "stds": stds,
+        "stderrs": stderrs,
         "runtimes": runtimes
     }